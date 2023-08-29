9fae4764d5c1a62e99e69025cb3d0daee10f0a35
brightigiemokha <brightigiemokha@yahoo.com>
2023-08-29 08:45:47 +0000

    add todoitems.html and todoitems details page to templates. all connected to view and urls

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TodoitemsList
+from .views import TodoitemsList, TodoitemsDetail
 
 urlpatterns = [
-    path('', TodoitemsList.as_view(), name='todoitems')
+    path('', TodoitemsList.as_view(), name='todoitems'),
+    path('todo_items/<int:pk>/', TodoitemsDetail.as_view(), name='todo_items'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from .models import Todoitems
 
 class TodoitemsList(ListView):
+    model = Todoitems
+    todo_object_name = 'tasks'
+
+class TodoitemsDetail(DetailView):
     model = Todoitems
