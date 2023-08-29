3017d7ed2b02be7e1d93e605d49309fc15648417
brightigiemokha <brightigiemokha@yahoo.com>
2023-08-29 09:53:29 +0000

    add todo items create view

--- todoapp/views.py
+++ todoapp/views.py
@@ -2,13 +2,20 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from .models import Todoitems
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 class TodoitemsList(ListView):
     model = Todoitems
-    todo_object_name = 'todo'
+    todo_object_name = 'todos'
 
 
 class TodoitemsDetail(DetailView):
     model = Todoitems
     context_object_name = 'todo'
     template_name = 'todoapp/todo_detail.html'
+
+class TodoitemsCreate(CreateView):
+    model = Todoitems
+    field = '__all__'
+    success_url = reverse_lazy('todos')
