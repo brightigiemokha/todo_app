249706d643aa6c429a99a90e695539d2c534933b
brightigiemokha <brightigiemokha@yahoo.com>
2023-08-29 14:03:04 +0000

    add todoitemsupdate

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TodoitemsList, TodoitemsDetail, TodoitemsCreate
+from .views import TodoitemsList, TodoitemsDetail, TodoitemsCreate, TodoitemsUpdate
 
 urlpatterns = [
     path('', TodoitemsList.as_view(), name='todos'),
-    path('todo/<int:pk>/', TodoitemsDetail.as_view(), name='todoitems'),
+    path('todo/<int:pk>/', TodoitemsDetail.as_view(), name='todo'),
     path('todo-create/', TodoitemsCreate.as_view(), name='todo-create'),
+    path('todo-update/<int:pk>/',TodoitemsUpdate.as_view(), name='todo-update'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from .models import Todoitems
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 
 class TodoitemsList(ListView):
@@ -15,7 +15,14 @@ class TodoitemsDetail(DetailView):
     context_object_name = 'todo'
     template_name = 'todoapp/todo_detail.html'
 
+
 class TodoitemsCreate(CreateView):
+    model = Todoitems
+    fields = '__all__'
+    success_url = reverse_lazy('todos')
+
+
+class TodoitemsUpdate(UpdateView):
     model = Todoitems
     fields = '__all__'
     success_url = reverse_lazy('todos')
