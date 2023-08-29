a2fed90bd40068bb60914c99bfa9121adc89f9ee
brightigiemokha <brightigiemokha@yahoo.com>
2023-08-29 19:36:07 +0000

    add signup page and connect user to the todo list and auth

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import TodoitemsList, TodoitemsDetail, TodoitemsCreate, TodoitemsUpdate, DeleteView, CustomLoginView
+from .views import TodoitemsList, TodoitemsDetail, TodoitemsCreate, TodoitemsUpdate, DeleteView, CustomLoginView, SignupPage
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('signup/', SignupPage.as_view(next_page='login'), name='signup'),
 
     path('', TodoitemsList.as_view(), name='todoitemss'),
     path('todo/<int:pk>/', TodoitemsDetail.as_view(), name='todo'),
