f49f72a942ec79109c1380a6fc146cfc450b4648
brightigiemokha <brightigiemokha@yahoo.com>
2023-08-29 14:08:38 +0000

    add deleteview class

--- todoapp/views.py
+++ todoapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from .models import Todoitems
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 class TodoitemsList(ListView):
@@ -25,4 +25,10 @@ class TodoitemsCreate(CreateView):
 class TodoitemsUpdate(UpdateView):
     model = Todoitems
     fields = '__all__'
+    success_url = reverse_lazy('todos')
+
+
+class DeleteView(DeleteView):
+    model = Todoitems
+    context_object_name = 'todo'
     success_url = reverse_lazy('todos')
