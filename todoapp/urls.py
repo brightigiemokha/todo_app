d7c0b3f34acf97a3181f7bb70a2c8e4b5f9e32c0
brightigiemokha <brightigiemokha@yahoo.com>
2023-08-29 14:55:03 +0000

    add auth to user

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TodoitemsList, TodoitemsDetail, TodoitemsCreate, TodoitemsUpdate
+from .views import TodoitemsList, TodoitemsDetail, TodoitemsCreate, TodoitemsUpdate, DeleteView
 
 urlpatterns = [
     path('', TodoitemsList.as_view(), name='todos'),
     path('todo/<int:pk>/', TodoitemsDetail.as_view(), name='todo'),
     path('todo-create/', TodoitemsCreate.as_view(), name='todo-create'),
-    path('todo-update/<int:pk>/',TodoitemsUpdate.as_view(), name='todo-update'),
+    path('todo-update/<int:pk>/', TodoitemsUpdate.as_view(), name='todo-update'),
+    path('todo-delete/<int:pk>/', DeleteView.as_view(), name='todo-delete'),
 ]
